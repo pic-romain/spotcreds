27aac697bf939c1b9907b8eb1ce7878a4bd007bd
Romain Pic <58364088+rpic84@users.noreply.github.com>
2021-09-18 11:09:06 +0200

    update

--- Scripts/test.py
+++ Scripts/test.py
@@ -49,10 +49,10 @@ for p in [all_playlists[0]]:
     playlist_id = p["playlist_id"]
     artist_name = p["artist_name"]
     live_playlist = spotify_api.get_playlist_track_uris(playlist_id).json()
-    live_tracks = {t["uri"]:t["name"] for t in live_playlist["tracks"]}
+    live_tracks = [t["uri"] for t in live_playlist["tracks"]]
     
     for i in range(p["tracks_uris"]):
-        if p["tracks_uris"][i] not in live_tracks.keys():
+        if p["tracks_uris"][i] not in live_tracks:
             print(p["tracks"][i]["name"], p["tracks_uris"][i])
     # removed_tracks = [t[0] for t in live_tracks if t[1] not in  ]
 
